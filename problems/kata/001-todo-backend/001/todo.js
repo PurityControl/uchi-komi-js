7f641cdf7fa3c1507c50e4bad02ec6ee2f9a13d7
Craig Ferry <PurityControl@users.noreply.github.com>
2016-11-17 15:23:50 +0000

    Copy get all logic into a getAll function.

--- problems/kata/001-todo-backend/001/todo.js
+++ problems/kata/001-todo-backend/001/todo.js
@@ -3,4 +3,14 @@ var db = nano.db.use('todo');
 
 var todo = {};
 
+todo.getAll = function() {
+  var results = [];
+  db.view('todos', 'all_todos', function(err, body) {
+    for (var row of body.rows) {
+      results.push(row.value);
+    }
+    return results;
+  });
+};
+
 module.exports = todo;
