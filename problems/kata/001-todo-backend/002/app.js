2416100f19979118e075c9e37be4e3cbd2a068ca
Craig Ferry <PurityControl@users.noreply.github.com>
2016-11-22 13:01:23 +0000

    Require body-parser to parse body parameters.

--- problems/kata/001-todo-backend/002/app.js
+++ problems/kata/001-todo-backend/002/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
-
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
 var app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', function(req, res, next) {
   console.log('GET / called');
