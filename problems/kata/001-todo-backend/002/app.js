27ae318c731f95a7669b9dd08ffc61d0ec75ca91
PurityControl <PurityControl@users.noreply.github.com>
2016-11-26 18:11:32 +0000

    Call update function from a router.

--- problems/kata/001-todo-backend/002/app.js
+++ problems/kata/001-todo-backend/002/app.js
@@ -63,21 +63,14 @@ app.get('/:url', function(req, res, next) {
 
 app.patch('/:url', function(req, res, next) {
   console.log('PATCH /' + req.params.url);
-  pool.query(
-    'select * from todo where id = $1',
-    [req.params.url],
-    function(err, result) {
-      var todo = result.rows[0];
-      var title = req.body.title || todo.title;
-      var completed = req.body.completed || todo.completed;
-      var order = req.body.order || todo.order
-      pool.query(
-        'update todo set title=$1, completed=$2, "order"=$3 where id=$4 returning *',
-        [title, completed, order, todo.id],
-        function(err, result) {
-          res.json(result.rows[0]);
-        });
-    });
+  todo.update(req.params.url, req.body, function(err, result) {
+    if (err) {
+      res.status = 500;
+      res.json({error: 'Cannot update the todo'});
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 app.delete('/:url', function(req, res, next) {
